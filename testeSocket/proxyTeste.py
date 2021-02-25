0b3c6b248ae923003d585d0cd8336794e511a97d
Rafael Costa <rafaelcostaf_@bitbucket.com>
2021-02-25 10:35:09 -0300

    alterações no proxy - resolvendo alguns bugs ainda

--- testeSocket/proxyTeste.py
+++ testeSocket/proxyTeste.py
@@ -17,10 +17,13 @@ import socket
 import threading
 
 serverHOST = 'localhost'
+serverPORT = 3300
+agentPORT = 3400
+FORMAT = 'utf-8'
+
+
 
 # CONNECTING TO AGENT
-
-agentPORT = 3300
 
 agentProxyAddress = (serverHOST, agentPORT)
 agentSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,12 +35,7 @@ agentConnection, _ = agentSock.accept()
 print('Agent connected')
 
 
-
-
 # CONNECTING TO SERVER
-
-
-serverPORT = 3100
 
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSock.connect((serverHOST, serverPORT))
@@ -45,43 +43,72 @@ serverSock.connect((serverHOST, serverPORT))
 print("Connected to server on port : " + str(serverPORT))
 
 
-        
+# FUNCTIONS
 
-agentMessage = ''.encode()
-serverMessage = ''.encode()     
-
-def receivingFromAgent():
-    while True:
-        agentMessage = agentConnection.recv(4)
-        print('Receive from agent')
-        
-def receivingFromServer():
-    while True:
-        serverMessage = serverSock.recv(4)
-        print('Receive from server')            
+agentMessage = 'ELE ESTÁ ENVIANDO ESSA MENSAGEM SEM ATUALIZAR'.encode()
+serverMessage = 'ELE ESTÁ ENVIANDO ESSA MENSAGEM SEM ATUALIZAR'.encode()
 
 def sendToServer():
-    while True:
-        serverSock.sendall(agentMessage)
-        print('Send to server')
+    # while True:
+    try:
+        print('[AGENT -> SERVER]' + agentMessage.decode())
+    except:
+        print('[AGENT -> SERVER]couldn\'t decode' )
+    serverSock.sendall(agentMessage)
+    # print('[AGENT]' + str(agentMessage.decode(FORMAT)))
 
 def sendToAgent():
+    # while True:
+    try:
+        print('[SERVER -> AGENT]' + serverMessage.decode())
+    except:
+        print('[SERVER -> AGENT]couldn\'t decode' )
+    agentConnection.sendall(serverMessage)
+    # print('[SERVER]' + str(serverMessage.decode(FORMAT)))
+
+def receivingFromAgent():
+    global agentMessage
     while True:
-        agentConnection.sendall(serverMessage)
-        print('Send to agent')
+        agentMessage = agentConnection.recv(1024)
+        try:
+            print('[AGENT]' + str(agentMessage.decode()))
+        except:
+            print('[AGENT]couldn\'t decode')
+        threadSendServer = threading.Thread(target=sendToServer)
+        threadSendServer.start()
+        
+def receivingFromServer():
+    global serverMessage
+    while True:
+        serverMessage = serverSock.recv(1024)
+        try:
+            print('[SERVER]' + str(serverMessage.decode())) 
+        except:
+            print('[SERVER]couldn\'t decode')
+        threadSendAgent = threading.Thread(target=sendToAgent)
+        threadSendAgent.start()
+           
+
+
 
 def start():
     threadRcvAgent = threading.Thread(target=receivingFromAgent)
     threadRcvServer = threading.Thread(target=receivingFromServer)
-    threadSendServer = threading.Thread(target=sendToServer) 
-    threadSendAgent = threading.Thread(target=sendToAgent)
+    # threadSendServer = threading.Thread(target=sendToServer) 
+    # threadSendAgent = threading.Thread(target=sendToAgent)
 
     threadRcvAgent.start()
-    threadSendAgent.start()
+    # threadSendAgent.start()
     threadRcvServer.start()
-    threadSendServer.start()
+    # threadSendServer.start()
 
 start()
+
+
+
+
+
+
 
 # while True:
 #     msg = agentConnection.recv(1024)
@@ -94,3 +121,47 @@ start()
 #     agentConnection.sendall(msg)
 #     print("Sent Agt")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 
+# Waiting agent to connect on port : 3400
+# Agent connected
+# Connected to server on port : 3300
+# [AGENT]+(scene rsg/agent/nao/nao_hetero.rsg 4)(syn)
+# [AGENT -> SERVER]+(scene rsg/agent/nao/nao_hetero.rsg 4)(syn)
+# Exception in thread Thread-2:
+# Traceback (most recent call last):
+#   File "/usr/lib/python3.6/threading.py", line 916, in _bootstrap_inner
+    # self.run()
+#   File "/usr/lib/python3.6/threading.py", line 864, in run
+    # self._target(*self._args, **self._kwargs)
+#   File "proxyTeste.py", line 75, in receivingFromServer
+    # print('[SERVER]' + str(serverMessage.decode()))
+# UnicodeDecodeError: 'utf-8' codec can't decode byte 0xeb in position 3: invalid continuation byte
+# 
+# ^CException ignored in: <module 'threading' from '/usr/lib/python3.6/threading.py'>
+# Traceback (most recent call last):
+#   File "/usr/lib/python3.6/threading.py", line 1294, in _shutdown
+    # t.join()
+#   File "/usr/lib/python3.6/threading.py", line 1056, in join
+    # self._wait_for_tstate_lock()
+#   File "/usr/lib/python3.6/threading.py", line 1072, in _wait_for_tstate_lock
+    # elif lock.acquire(block, timeout):
+# KeyboardInterrupt
+# 
