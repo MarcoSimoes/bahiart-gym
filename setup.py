da9ebdd776b5ffbfd9ae0ed5dd1d39796302d7eb
BahiaRT <msimoes@uneb.br>
2022-04-21 14:50:20 -0300

    Fixes in README.md and setup.py and updated MANIFES.in.

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+#from distutils.core import setup
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
 setup(
   name = 'bahiart_gym',      
   packages = ['bahiart_gym'],   
-  version = '1.0.1',     
+  version = '1.0.3',      
   license='agpl-3.0',        
-  description = 'A toolkit to develop openAI Gym environments on top of the RCSSSERVER3D simulator',   
+  description = 'A toolkit to develop openAI Gym environments on top of the RCSSSERVER3D simulator',
+  long_description=long_description,
+  long_description_content_type="text/markdown",   
   author = 'Gabriel Mascarenhas, Marco A. C. Simões, Rafael Fonseca',                  
   author_email = 'teambahiart@gmail.com',     
   url = 'https://bitbucket.org/bahiart3d/bahiart-gym/',   
