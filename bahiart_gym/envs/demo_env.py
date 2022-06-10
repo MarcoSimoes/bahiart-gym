6ba8151b2ac8cd9a4fe46b93e0dc55f4ed4d3433
BahiaRT <msimoes@uneb.br>
2022-06-10 16:57:58 -0300

    Updated classes with the Copyright License comment. 	modified:   bahiart_gym/__init__.py 	modified:   bahiart_gym/envs/__init__.py 	modified:   bahiart_gym/envs/demo_env.py 	modified:   bahiart_gym/server/__init__.py 	modified:   bahiart_gym/server/comms.py 	modified:   bahiart_gym/server/proxy.py 	modified:   bahiart_gym/server/singleton.py 	modified:   bahiart_gym/server/trainer.py 	modified:   bahiart_gym/server/world.py 	modified:   bahiart_gym/start.py 	modified:   bahiart_gym/terminate.py 	modified:   demo_test.py 	modified:   setup.py

--- bahiart_gym/envs/demo_env.py
+++ bahiart_gym/envs/demo_env.py
@@ -1,5 +1,22 @@
-#import sys
-#sys.path.append("../")
+"""
+        Copyright (C) 2022  Salvador, Bahia
+        Gabriel Mascarenhas, Marco A. C. Simões, Rafael Fonseca
+
+        This file is part of BahiaRT GYM.
+
+        BahiaRT GYM is free software: you can redistribute it and/or modify
+        it under the terms of the GNU Affero General Public License as
+        published by the Free Software Foundation, either version 3 of the
+        License, or (at your option) any later version.
+
+        BahiaRT GYM is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU Affero General Public License for more details.
+
+        You should have received a copy of the GNU Affero General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import gym
 import numpy as np
 from gym import spaces
@@ -9,8 +26,6 @@ from bahiart_gym.server.world import World
 from bahiart_gym.agentcomms import AgentComms
 from bahiart_gym.agentcomms import InvalidHostAndPortLengths
 
-#sys.path.append("../server")
-#from bahiart_gym.server import *
 
 class DemoEnv(gym.Env):
     metadata = {'render.modes': ['human']}
